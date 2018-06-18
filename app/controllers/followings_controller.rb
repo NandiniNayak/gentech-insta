708d84e3ff7071b8ffeac3ccad520874a9a3ae8d
Nandini Nayak <nandini.r.nayak@gmail.com>
2018-06-18 16:28:53 +1000

    follow page updated with follow and unfollow buttons

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -7,7 +7,8 @@ class FollowingsController < ApplicationController
     redirect_to follow_page_path
   end
   def destroy
-    @following = current_user.followings.find(params[:id])
+    # use find_by instead of find, as you are passing the user id
+    @following = current_user.followings.find_by(:follower_id => params[:id])
     @following.destroy
     flash[:notice] = "unfollowed"
     redirect_to follow_page_path
